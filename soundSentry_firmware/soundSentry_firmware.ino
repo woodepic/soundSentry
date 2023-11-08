48aa5aa577546dca616d2d572c90167790e06af5
Matt Wood <matt@Matts-16-M1.local>
2023-11-08 13:14:06 -0700

    Avg mic measurements added. Still doesn't measure loudness

--- soundSentry_firmware/soundSentry_firmware.ino
+++ soundSentry_firmware/soundSentry_firmware.ino
@@ -11,25 +11,47 @@ void setup() {
   lcd.begin(16, 2); //cols, rows
   lcd.setCursor(0, 0);
   lcd.print("hello, world!");
+  Serial.begin(9600);
 
   pinMode(mic1, INPUT);
+  pinMode(mic2, INPUT);
+  pinMode(mic3, INPUT);
 }
 
 void loop() {
   lcd.setCursor(0, 1);
 
-  int volume = analogRead(mic1);
+  long volume1 = analogRead(mic1);
+  long volume2 = analogRead(mic2);
+  long volume3 = analogRead(mic3);
   
-  for (int i = 0; i < 20; i++) {
-    // This loop will run 20 times. i will take values from 0 to 19.
-    // You can place your code that you want to execute 20 times here.
+  long avgVol1 = 0;
+  long avgVol2 = 0;
+  long avgVol3 = 0;
+  int count = 0;
+  long measStart = millis();
+  for (int i = 0; i < 100; i++) {
+    avgVol1 += + analogRead(mic1);
+    avgVol2 += + analogRead(mic2);
+    avgVol3 += + analogRead(mic3);
+    count++;
   }
+  long measEnd = millis();
+  long measTime = measEnd - measStart;
+  avgVol1 = avgVol1 / count;
+  avgVol2 = avgVol2 / count;
+  avgVol3 = avgVol3 / count;
 
-  lcd.clear();
-  lcd.print(volume);
+  // lcd.clear();
+  // lcd.print(volume);
 
   delay(100);
+  Serial.println("Mic1: " + String(avgVol1) + "mV");
+  Serial.println("Mic2: " + String(avgVol2) + "mV");
+  Serial.println("Mic3: " + String(avgVol3) + "mV");
+  Serial.println("Measurement Time: " + String(measTime) + "ms");
+  Serial.println("");
 
-  int db = map(peakToPeak,20,4096,49.5,94);             //calibrate for deciBels
+  // int db = map(peakToPeak,20,4096,49.5,94);             //calibrate for deciBels
 
 }
