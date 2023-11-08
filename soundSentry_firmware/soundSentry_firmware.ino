ea4914003715d9936e59cb8fac360312549a1b00
Matt Wood <matt@Matts-16-M1.local>
2023-11-07 18:43:34 -0700

    temporary save, cannot seem to get good readings from the mics

--- soundSentry_firmware/soundSentry_firmware.ino
+++ soundSentry_firmware/soundSentry_firmware.ino
@@ -4,13 +4,32 @@
 const int rs = 8, en = 7, d4 = 12, d5 = 11, d6 = 10, d7 = 9;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
+//Initialize Mics
+const int mic1 = A2, mic2 = A0, mic3 = A1;
+
 void setup() {
   lcd.begin(16, 2); //cols, rows
   lcd.setCursor(0, 0);
   lcd.print("hello, world!");
+
+  pinMode(mic1, INPUT);
 }
 
 void loop() {
   lcd.setCursor(0, 1);
-  lcd.print(millis() / 1000);
+
+  int volume = analogRead(mic1);
+  
+  for (int i = 0; i < 20; i++) {
+    // This loop will run 20 times. i will take values from 0 to 19.
+    // You can place your code that you want to execute 20 times here.
+  }
+
+  lcd.clear();
+  lcd.print(volume);
+
+  delay(100);
+
+  int db = map(peakToPeak,20,4096,49.5,94);             //calibrate for deciBels
+
 }
